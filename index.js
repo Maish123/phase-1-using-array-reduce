571fbfe1568a0fbed8ac9ebe26e6a6619b11ada4
Denis Maina <Denis.maina@student.moringaschool.com>
2022-06-12 12:47:35 +0300

    updated file

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function addBatteries(accumulator, element){
+    return accumulator + element;
+  }
+  
+  const totalBatteries = batteryBatches.reduce(addBatteries);
+  
+  console.log(totalBatteries);
+
+
+
